300c7b4e0830fb44d8f7658d133e00bc7b52c110
tvg001 <39940725+tvg001@users.noreply.github.com>
2022-11-10 18:53:14 +0530

    Update InnerClassInheritanceV01.java

--- InnerClassInheritanceV01.java
+++ InnerClassInheritanceV01.java
@@ -17,7 +17,7 @@ class OuerPlus extends Zuer{
 public class InnerClassInheritanceV01 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Outer out = new Outer();
+        //Outer out = new Outer();
         OuerPlus oplus = new OuerPlus();
         //oplus.Inner inC = oplus.new Inner(); tried  to access Inner class  similar to field
         OuerPlus.Inner  in = oplus.new Inner();
